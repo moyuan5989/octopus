0d9983df979af1942ee3bdd7c3c383b9e3dc6555
moyuan5989 <Jiekai@wangjiekaideMacBook-Pro.local>
2016-04-10 14:54:21 -0700

    add platform

--- DenemeGameWithBurak/GameScene.swift
+++ DenemeGameWithBurak/GameScene.swift
@@ -858,8 +858,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             TimerAddMine = NSTimer.scheduledTimerWithTimeInterval(4.245, target: self, selector: Selector("AddMine"), userInfo: nil, repeats: true)
             TimerAddCube = NSTimer.scheduledTimerWithTimeInterval(6.245, target: self, selector: Selector("AddCube"), userInfo: nil, repeats: true)
-            TimerAddDown = NSTimer.scheduledTimerWithTimeInterval(4.5, target: self, selector: Selector("AddDown"), userInfo: nil, repeats: true)
-            TimerAddPlat = NSTimer.scheduledTimerWithTimeInterval(4.5, target: self, selector: Selector("AddPlat"), userInfo: nil, repeats: true)
+            TimerAddDown = NSTimer.scheduledTimerWithTimeInterval(8.5, target: self, selector: Selector("AddDown"), userInfo: nil, repeats: true)
+            TimerAddPlat = NSTimer.scheduledTimerWithTimeInterval(2.5, target: self, selector: Selector("AddPlat"), userInfo: nil, repeats: true)
             
             TimerAddElectriclighting = NSTimer.scheduledTimerWithTimeInterval(5.234, target: self, selector: Selector("AddElectriclighting"), userInfo: nil, repeats: true)
             TimerRoketAdd = NSTimer.scheduledTimerWithTimeInterval(7.743, target: self, selector: Selector("RoketAdd"), userInfo: nil, repeats: true)
@@ -873,7 +873,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             TimerAddMine = NSTimer.scheduledTimerWithTimeInterval(5.245, target: self, selector: Selector("AddMine"), userInfo: nil, repeats: true)
             TimerAddCube = NSTimer.scheduledTimerWithTimeInterval(5.45, target: self, selector: Selector("AddCube"), userInfo: nil, repeats: true)
             TimerAddDown = NSTimer.scheduledTimerWithTimeInterval(3.65, target: self, selector: Selector("AddDown"), userInfo: nil, repeats: true)
-            TimerAddPlat = NSTimer.scheduledTimerWithTimeInterval(3.65, target: self, selector: Selector("AddPlat"), userInfo: nil, repeats: true)
+            TimerAddPlat = NSTimer.scheduledTimerWithTimeInterval(2.55, target: self, selector: Selector("AddPlat"), userInfo: nil, repeats: true)
             TimerAddElectriclighting = NSTimer.scheduledTimerWithTimeInterval(3.234, target: self, selector: Selector("AddElectriclighting"), userInfo: nil, repeats: true)
             TimerRoketAdd = NSTimer.scheduledTimerWithTimeInterval(11.743, target: self, selector: Selector("RoketAdd"), userInfo: nil, repeats: true)
             
@@ -887,7 +887,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             TimerAddMine = NSTimer.scheduledTimerWithTimeInterval(2.945, target: self, selector: Selector("AddMine"), userInfo: nil, repeats: true)
             TimerAddCube = NSTimer.scheduledTimerWithTimeInterval(5.945, target: self, selector: Selector("AddCube"), userInfo: nil, repeats: true)
             TimerAddDown = NSTimer.scheduledTimerWithTimeInterval(3.45, target: self, selector: Selector("AddDown"), userInfo: nil, repeats: true)
-            TimerAddPlat = NSTimer.scheduledTimerWithTimeInterval(3.45, target: self, selector: Selector("AddPlat"), userInfo: nil, repeats: true)
+            TimerAddPlat = NSTimer.scheduledTimerWithTimeInterval(2.45, target: self, selector: Selector("AddPlat"), userInfo: nil, repeats: true)
             TimerAddElectriclighting = NSTimer.scheduledTimerWithTimeInterval(3.034, target: self, selector: Selector("AddElectriclighting"), userInfo: nil, repeats: true)
             TimerRoketAdd = NSTimer.scheduledTimerWithTimeInterval(10.543, target: self, selector: Selector("RoketAdd"), userInfo: nil, repeats: true)
             
@@ -1039,8 +1039,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         
         Plat = SKSpriteNode(texture: PlatTexture1)
-        let minesRandom = arc4random() % UInt32(2)
-        if minesRandom == 0
+        let platsRandom = arc4random() % UInt32(2)
+        if platsRandom == 0
         {
             Plat = SKSpriteNode(texture: PlatTexture1)
         }else
@@ -1048,30 +1048,45 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             Plat = SKSpriteNode(texture: PlatTexture2)
             
         }
-        Plat.size.width = 200
-        Plat.size.height = 40
-        Plat.position = CGPoint(x: self.frame.size.width + 0, y: 270)
-        let moveMayinX = SKAction.moveToX ( -self.frame.size.width / 2 , duration: 4)
+        Plat.size.width = 300
+        Plat.size.height = 50
+        
+        let plat_random = arc4random_uniform(3)
+        
+//        var low_height = 800
+        
+        if plat_random == 0{
+            Plat.position = CGPoint(x: self.frame.size.width   , y: self.frame.size.height - 300)
+        }else if plat_random == 1{
+            Plat.position = CGPoint(x: self.frame.size.width   , y: self.frame.size.height - 500)
+        }else if plat_random == 2{
+            Plat.position = CGPoint(x: self.frame.size.width   , y: self.frame.size.height - 350)
+        }else if plat_random == 3{
+            Plat.position = CGPoint(x: self.frame.size.width   , y: self.frame.size.height - 450)
+        }
+        
+        
+//        Plat.zPosition = 1.1
+        
+
+        let moveMayinX = SKAction.moveToX ( -self.frame.size.width / 4 , duration: 4)
         Plat.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: Plat.size.width - 40 , height: Plat.size.height - 30))
         
-        Plat.physicsBody?.categoryBitMask = objectgroup
+//        Plat.physicsBody?.categoryBitMask = objectgroup
         
         Plat.physicsBody?.dynamic = false
-        Plat.physicsBody?.affectedByGravity = false
-        Plat.physicsBody?.allowsRotation = false
-        
-        
         
         let removeAction = SKAction.removeFromParent()
         let dusmanMovebgForever = SKAction.repeatActionForever(SKAction.sequence([moveMayinX,removeAction]))
         
         
         
-        //        animations.rotateAnimationsToAngle(Cube, animDuration: 0.2)
-        Cube.runAction(dusmanMovebgForever)
-        Cube.zPosition = 1
+//        animations.rotateAnimationsToAngle(Plat, animDuration: 0.2)
+        Plat.runAction(dusmanMovebgForever)
+        Plat.zPosition = 1
         
         movingObject.addChild(Plat)
+    
     }
     
 
